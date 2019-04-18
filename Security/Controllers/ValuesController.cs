99d0779a209b1b659b29fc77367ce0cd3ac8e7f9
Alejandro Pena <alejandropena17@gmail.com>
2019-04-17 20:54:16 -0400

    NEGLIGABLE changes, testing Entity setup and saving. Notes and DB env fix

--- Security/Controllers/ValuesController.cs
+++ Security/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Security.Models;
 
 namespace Security.Controllers
 {
@@ -10,6 +11,12 @@ namespace Security.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly SecurityContext _context; //Entity Context
+
+        public ValuesController(SecurityContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -24,10 +31,26 @@ namespace Security.Controllers
             return "value";
         }
 
+        /* todo can save whole SEcurity with prices array filled...
+                    realistically will need to create Security then update with list of prices, 
+                    front end likely just add one a day..mimick real life */
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost] 
+        public async Task<IActionResult> Post([FromBody] Models.Security security)
         {
+            try
+            {
+                _context.Securities.Add(security);
+                _context.SaveChanges();
+                return new OkObjectResult("Succesfully saved todo");
+            }
+            catch (Exception e)
+            {
+                //normally a log
+                Console.WriteLine(e);
+                return new BadRequestObjectResult("No Luck");
+            }
+        
         }
 
         // PUT api/values/5
--- Security/Models/Security.cs
+++ Security/Models/Security.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
+// todo Nice to have: Angular graphs generator of prices, admin sign ins for 'official' updates. Clean UI..
+// Authentication/roles HttpContext, service token?, env appsettings...Twilio API example
 namespace Security.Models
 {
     public class Security
@@ -10,11 +12,11 @@ namespace Security.Models
         // todo [required] ?
         [Key]
         [JsonIgnore]
-        public Int32 Id { get; set; } // todo auto increment in DB
+        public Int32 Id { get; set; } // auto increment in DB
         
         public string SecurityName { get; set; } // 25 char
         
-        public string ISIN { get; set; } // 12 char alphanumeric
+        public string ISIN { get; set; } // 12 char alphanumeric, todo restrict to 12 num exactly?
         
         public string Country { get; set; } // 25 char
         
